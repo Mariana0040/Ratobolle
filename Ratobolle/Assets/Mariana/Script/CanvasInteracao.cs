05c7c2de0ab0a05763b8f2a5cfff0818104c3119
Mariana Santos Silva <marisantosilva0706@gmail.com>
2025-09-10 12:15:27 -0300

    PedidosPizza

---
+++ Ratobolle/Assets/Mariana/Script/CanvasInteracao.cs
@@ -0,0 +1,52 @@
+// Salve como CanvasInteracao.cs
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class CanvasInteracao : MonoBehaviour
+{
+    [Header("Referências")]
+    public GameObject painelBalao; // O objeto pai que contém o fundo e os emojis
+    public Transform containerDePedidos; // O objeto com o Horizontal Layout Group
+    public GameObject prefabIconePedido; // O prefab do Image para o emoji
+
+    private Transform cameraTransform;
+
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        painelBalao.SetActive(false);
+    }
+
+    // Faz o canvas sempre olhar para a câmera
+    void LateUpdate()
+    {
+        if (painelBalao.activeSelf)
+        {
+            transform.LookAt(transform.position + cameraTransform.rotation * Vector3.forward, cameraTransform.rotation * Vector3.up);
+        }
+    }
+
+    public void MostrarPedido(List<ReceitaSO> pedido)
+    {
+        // Limpa pedidos antigos
+        foreach (Transform child in containerDePedidos)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // Cria os novos ícones
+        foreach (var receita in pedido)
+        {
+            GameObject icone = Instantiate(prefabIconePedido, containerDePedidos);
+            icone.GetComponent<Image>().sprite = receita.emojiPrato;
+        }
+
+        painelBalao.SetActive(true);
+    }
+
+    public void EsconderBalao()
+    {
+        painelBalao.SetActive(false);
+    }
+}
--- Ratobolle/Assets/Mariana/Script/GerenciadorRestaurante.cs
+++ Ratobolle/Assets/Mariana/Script/GerenciadorRestaurante.cs
@@ -11,6 +11,18 @@ public class GerenciadorRestaurante : MonoBehaviour
     [Tooltip("Arraste para cá todos os objetos que têm o script 'Chair'.")]
     public List<Chair> todasAsChairs; // Mantenha esta lista pública para arrastar no Inspector
 
+    [Header("Progressão e Níveis")]
+    public int nivelAtualRestaurante = 1;
+    public int pontosParaProximoNivel = 3; // Quantos clientes para um especial
+    [SerializeField] private int pontosAtuais = 0;
+
+    [Header("Banco de Receitas por Nível")]
+    public List<ReceitaSO> receitasNivel1;
+    public List<ReceitaSO> receitasNivel2; //*!!!!!!!!!*
+    public List<ReceitaSO> receitasNivel3;
+    public List<ReceitaSO> receitasNivel4;
+    // Adicione mais listas para mais níveis...
+
     [Header("Controle de Paciência")]
     public int maxEntradasNaCozinha = 3;
     private int entradasAtuaisNaCozinha = 0;
@@ -26,6 +38,33 @@ public class GerenciadorRestaurante : MonoBehaviour
         else
         {
             Instance = this;
+        }
+    }
+
+    // Chamado pelo jogador quando uma entrega é bem-sucedida
+    public void RegistrarEntregaSucedida()
+    {
+        pontosAtuais++;
+        if (pontosAtuais >= pontosParaProximoNivel)
+        {
+            pontosAtuais = 0;
+            nivelAtualRestaurante++;
+            Debug.Log($"<color=yellow>NÍVEL DO RESTAURANTE AUMENTOU PARA {nivelAtualRestaurante}!</color>");
+            // Aqui você pode invocar o cliente especial ou liberar novas receitas
+        }
+    }
+
+    // Clientes perguntam quais receitas eles podem pedir
+    public List<ReceitaSO> ObterReceitasDisponiveis()
+    {
+        switch (nivelAtualRestaurante)
+        {
+            case 1: return receitasNivel1;
+            case 2: return receitasNivel2;
+            case 3: return receitasNivel3;
+            case 4: return receitasNivel4;
+            // etc...
+            default: return receitasNivel1;
         }
     }
 
